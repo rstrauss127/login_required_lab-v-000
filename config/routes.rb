0e6b1c41d45240e460a9315b27f4c2c2c59f2035
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-16 21:41:56 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
-  get '/show' => 'sessions#show'
+  get '/show' => 'secrets#show'
 end
