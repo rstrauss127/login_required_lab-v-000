c0e575915facbeddebf820c91ac401c11e400310
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-16 21:31:41 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -7,8 +7,8 @@ class SecretsController < ApplicationController
     end
   end
 
-  private
-  def require_login
-    return head(:forbidden) unless session.include? :name
-  end
+#  private
+  #def require_login
+  #  return head(:forbidden) unless session.include? :name
+  #end
 end
