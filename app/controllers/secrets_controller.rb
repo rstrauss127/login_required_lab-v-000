a368ed62e6d32950cbe51e224543616f151ab09d
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-16 21:46:33 +0000

    Done.

---
+++ app/controllers/secrets_controller.rb
@@ -0,0 +1,15 @@
+class SecretsController < ApplicationController
+  before_action :require_login
+  skip_before_action :require_login, only: [:show]
+
+  def show
+    unless session.include? :name
+      redirect_to '/login'
+    end
+  end
+
+  private
+  def require_login
+    return head(:forbidden) unless session.include? :name
+  end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if params[:name].blank?
+      redirect_to '/login'
+    else
+      session[:name] = params[:name]
+    end
+  end
+
+  def destroy
+    session.delete :name
+  end
+end
