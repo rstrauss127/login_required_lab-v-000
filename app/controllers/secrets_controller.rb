2c4b2a8d415dba9ce379094ae15deb3b14cf0374
Rebecca Strauss <rstrauss127@gmail.com>
2019-05-16 21:31:28 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,7 +2,9 @@ class SecretsController < ApplicationController
   before_action :require_login
 
   def show
-
+    unless session.include? :name
+      redirect_to '/login'
+    end
   end
 
   private
